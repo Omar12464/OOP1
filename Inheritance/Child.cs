56585a1f01b0a351580ca46270f8b71cc87a9e33
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-07-08 18:12:38 +0300

    Part 1

---
+++ Inheritance/Child.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1.Inheritance
+{
+    internal class Child : Parent
+    {
+        public int Z
+        {
+            get;
+            set;
+
+        }
+
+        public Child(int _x,int _y,int z):base(_x,_y)
+        {
+            this.Z = z;
+
+        }
+
+        public new int Product()
+        {
+            return X*Y*Z;
+        }
+        public override string ToString()
+        {
+            return $"{X},{Y},{Z}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP1.Inheritance;
+using System;
 
 namespace OOP1
 {
@@ -6,9 +7,45 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Part 1
+            ////int size = 5;
+            ////PhoneBook phoneBook = new PhoneBook(5);
+            ////phoneBook.addPerson(1, "Omar", 111);
+            ////phoneBook.addPerson(2, "Mido", 123);
+            ////phoneBook.addPerson(3, "Body", 115);
+            //////Console.WriteLine(phoneBook.getNumber("Omar"));
+            //////phoneBook.SetNumber("Omar", 15236);
+            //////Console.WriteLine(phoneBook.getNumber("Omar")); 
+            ////phoneBook["Omar"] = 1235;
+            ////Console.WriteLine(phoneBook["Omar"]);
+            ////string Name = "mm";
+            ////Console.WriteLine(Name[0]);
+            ////for (int i = 0; i <= phoneBook.Size-1; i++)
+            ////{
+            ////    Console.WriteLine(phoneBook[i]);
+            ////}
 
-            Console.WriteLine("");
+            //Car c1;
+            //c1 = new Car(10,"BM",400);
+            //Console.WriteLine(c1.ToString());
+            //c1 = new Car(15);
+            //Console.WriteLine(c1.ToString());
+
+            //Parent parent=new Parent(1,3);
+            //Console.WriteLine(parent.Product()) ;
+            //Console.WriteLine(parent.ToString());
+            //Child C2 = new Child(1,2,3);
+            //Console.WriteLine(C2.ToString());
+            //Console.WriteLine(C2.Product());
+
+            #endregion
+
+            #region Part2
+
+
+
+            #endregion
+
         }
     }
 }
